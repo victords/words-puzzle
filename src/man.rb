b27b0c36012166afbaa5d58bce0e636af17b8fb1
Victor David Santos <victordavidsantos@gmail.com>
2021-09-03 08:58:12 -0300

    Wand spell animation, keep physics working during spell.

--- src/man.rb
+++ src/man.rb
@@ -11,6 +11,7 @@ class Man
 
   ANIM_IDLE_CYCLE = 90
   ANIM_WALK_CYCLE = 40
+  ANIM_SPELL_CYCLE = 60
 
   include Movement
 
@@ -47,6 +48,7 @@ class Man
   end
 
   def update(screen)
+    speed = Vector.new
     if @spell
       if KB.key_pressed?(Gosu::KB_X)
         @spell = nil
@@ -65,22 +67,24 @@ class Man
       elsif KB.key_pressed?(Gosu::KB_UP)
         change_spell_word(1)
       end
-      return
-    end
-
-    speed = Vector.new
-    if KB.key_down?(Gosu::KB_RIGHT)
-      speed.x += MOVE_FORCE
-    elsif KB.key_down?(Gosu::KB_LEFT)
-      speed.x -= MOVE_FORCE
-    else
       speed.x = -BRAKE_RATE * @speed.x
+    else
+      if KB.key_pressed?(Gosu::KB_Z) && @mana > 0
+        @spell = { obj: @spell_objs[0], prop: @spell_props[0], state: :obj }
+        @on_start_spell.call(@x, @y, @spell[:obj].to_s + 's', @spell[:prop].to_s)
+      elsif KB.key_down?(Gosu::KB_RIGHT)
+        speed.x += MOVE_FORCE
+      elsif KB.key_down?(Gosu::KB_LEFT)
+        speed.x -= MOVE_FORCE
+      else
+        speed.x = -BRAKE_RATE * @speed.x
+      end
     end
 
     stuck = (@left || @right)&.sticky?
     inside_liquid = screen.inside_liquid?(self)
 
-    if (stuck || inside_liquid || @bottom) && KB.key_pressed?(Gosu::KB_SPACE)
+    if (stuck || inside_liquid || @bottom) && KB.key_pressed?(Gosu::KB_SPACE) && @spell.nil?
       speed.y = -JUMP_FORCE
     elsif stuck
       @speed.y = 0 if @speed.y > 0
@@ -114,25 +118,30 @@ class Man
       @on_leave.call(:right)
     end
 
-    if KB.key_pressed?(Gosu::KB_Z) && @mana > 0
-      @spell = { obj: @spell_objs[0], prop: @spell_props[0], state: :obj }
-      @on_start_spell.call(@x, @y, @spell[:obj].to_s + 's', @spell[:prop].to_s)
-    end
-
     # ========================== animation ===========================
     walking = @speed.x.abs > G.min_speed.x
-    cycle_time = walking ? ANIM_WALK_CYCLE : ANIM_IDLE_CYCLE
+    cycle_time = @spell ? ANIM_SPELL_CYCLE : walking ? ANIM_WALK_CYCLE : ANIM_IDLE_CYCLE
     @anim_frame = 0 if @anim_frame > cycle_time
     rate = (@anim_frame >= cycle_time / 2 ? cycle_time - @anim_frame : @anim_frame).to_f / cycle_time
 
-    if walking
+    if @spell
+      @head_offset = rate * 3
+      @vest_offset = rate * 2
+      @eye_offset = 5
+      @hand_offset = [Vector.new(@vest_offset, @head_offset / 2 + 35), Vector.new(rate * 5, rate * 5 + 25)]
+      @wand_offset = [Vector.new(@hand_offset[1].x + 4, @hand_offset[1].y + 8), Vector.new(rate * 40 + 30, rate * 30 + 5)]
+    elsif walking
       @head_offset = rate * 4
       @vest_offset = rate * 5
       @eye_offset = @speed.x < 0 ? 8 : 2
+      @hand_offset = default_hand_offsets
+      @wand_offset = default_wand_offsets
     else
       @head_offset = rate * 4
       @vest_offset = rate * 3
       @eye_offset = 5
+      @hand_offset = default_hand_offsets
+      @wand_offset = default_wand_offsets
     end
 
     @anim_frame += 1
@@ -150,22 +159,39 @@ class Man
     @on_update_spell.call(word, @spell[word].to_s + (word == :obj ? 's' : ''))
   end
 
+  def default_hand_offsets
+    [Vector.new(@vest_offset, @head_offset / 2 + 35), Vector.new(@vest_offset, @head_offset / 2 + 35)]
+  end
+
+  def default_wand_offsets
+    [Vector.new(@hand_offset[1].x, @hand_offset[1].y + 13), Vector.new(32, @head_offset / 2 + 12)]
+  end
+
   def draw
+    # body
     G.window.draw_triangle(@x + @w / 2, @y + @head_offset + 5, Color::DARK_BLUE,
                            @x - @vest_offset, @y + @h, Color::DARK_BLUE,
                            @x + @w + @vest_offset, @y + @h, Color::DARK_BLUE, 0)
+
+    # head
     G.window.draw_circle(@x + 4, @y + @head_offset, 24, Color::BEIGE)
+
+    # eyes
     G.window.draw_rect(@x + @w / 2 - @eye_offset - 2, @y + @head_offset + 6, 4, 8, Color::BLACK)
     G.window.draw_rect(@x + @w / 2 - @eye_offset + 8, @y + @head_offset + 6, 4, 8, Color::BLACK)
+
+    # hat
     G.window.draw_triangle(@x + @w / 2, @y + @head_offset - 20, Color::DARK_BLUE,
                            @x + @w / 2 - 12, @y + @head_offset + 3, Color::DARK_BLUE,
                            @x + @w / 2 + 12, @y + @head_offset + 3, Color::DARK_BLUE, 0)
-    G.window.draw_circle(@x - 5 - @vest_offset, @y + @head_offset / 2 + 35, 12, Color::BEIGE)
-    G.window.draw_circle(@x + @w - 7 + @vest_offset, @y + @head_offset / 2 + 35, 12, Color::BEIGE)
+
+    # hands
+    G.window.draw_circle(@x - 5 - @hand_offset[0].x, @y + @hand_offset[0].y, 12, Color::BEIGE)
+    G.window.draw_circle(@x + @w - 7 + @hand_offset[1].x, @y + @hand_offset[1].y, 12, Color::BEIGE)
 
     # wand
-    e1 = Vector.new(@x + @w + @vest_offset, @y + @head_offset / 2 + 48)
-    e2 = Vector.new(@x + @w + 32, @y + @head_offset / 2 + 12)
+    e1 = Vector.new(@x + @w + @wand_offset[0].x, @y + @wand_offset[0].y)
+    e2 = Vector.new(@x + @w + @wand_offset[1].x, @y + @wand_offset[1].y)
     v = e2 - e1
     d = Math.sqrt(v.x * v.x + v.y * v.y)
     o = v.rotate(Math::PI * 0.5) / d
