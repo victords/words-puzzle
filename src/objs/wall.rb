540af218681b658366bf412d0319270f896a5b4c
Victor David Santos <victordavidsantos@gmail.com>
2021-09-10 08:48:09 -0300

    Better wall drawing.

--- src/objs/wall.rb
+++ src/objs/wall.rb
@@ -13,15 +13,20 @@ class Wall < Obj
 
   def draw
     G.window.draw_rect(@x, @y, @w, @h, COLOR1)
-    # h_count = (@w / 60).floor
-    # v_count = (@h / 40).floor
-    # (0...h_count).each do |i|
-    #   (0...v_count).each do |j|
-    #     next if (i + j).odd?
-    #     G.window.draw_rect(@x + i * 60 + 10, @y + j * 40 + 10, 50, 30, COLOR2)
-    #     G.window.draw_rect(@x + i * 60 + 5, @y + j * 40 + 5, 50, 30, COLOR3)
-    #   end
-    # end
+    G.window.draw_rect(@x + 2, @y + 2, @w - 4, @h - 4, COLOR2)
+    start_x = x = 50 * ((@x + 1) / 50)
+    y = 30 * ((@y + 1) / 30)
+    while y < @y + @h - 2
+      G.window.draw_rect(@x + 2, y, @w - 4, [2, @y + @h - 2 - y].min, Color::BLACK) if y >= @y + 2
+      x += 25 if (y / 30).even?
+      while x < @x + @w - 2
+        h = y >= @y + 2 ? [30, @y + @h - 2 - y].min : y + 28 - @y
+        G.window.draw_rect(x, [y, @y + 2].max, [2, @x + @w - 2 - x].min, h, Color::BLACK) if x >= @x + 2
+        x += 50
+      end
+      x = start_x
+      y += 30
+    end
 
     super
   end
