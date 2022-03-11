e8f1d1a721859c5564aeb6ec34352c2fd47521db
Victor David Santos <victordavidsantos@gmail.com>
2022-03-11 19:26:21 -0300

    Remove unused code.

--- src/constants.rb
+++ src/constants.rb
@@ -2,11 +2,7 @@ module Color
   BLACK = 0xff000000
   WHITE = 0xffffffff
   LIME = 0xff00ff00
-  RED = 0xffff0000
-  DARK_BLUE = 0xff000099
-  BEIGE = 0xffffee80
   GRAY = 0xff999999
-  GOLD = 0xffffdd00
   MAGENTA = 0xffff00ff
 end
 
@@ -39,7 +35,7 @@ class Graphics
                              5, 5, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
                              5, 3, 5, 5, 5, 5, 5, 5, 5, 5, 1, 2, 1, 2, 1, 5, 1, 5, 4, 4, 2, 2, 2, 2, 5, 3, 5, 1, 3, 7, 7,
                              9, 5, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 5, 5, 5, 5], 11, 3, 1)
-      @text_helper = TextHelper.new(@font, Graphics::SCALE, Graphics::SCALE)
+      @text_helper = TextHelper.new(@font, SCALE, SCALE)
     end
   end
 end
--- src/utils.rb
+++ src/utils.rb
@@ -7,40 +7,10 @@ class Utils
       (timer >= interval * 0.5 ? interval - timer : timer).to_f / (interval * 0.5)
     end
 
-    def lighten(color, rate)
-      a, r, g, b = components(color)
-      r = [(r + (255 - r) * rate).round, 255].min
-      g = [(g + (255 - g) * rate).round, 255].min
-      b = [(b + (255 - b) * rate).round, 255].min
-      compose(a, r, g, b)
-    end
-
-    def darken(color, rate)
-      a, r, g, b = components(color)
-      r = (r * (1 - rate)).round
-      g = (g * (1 - rate)).round
-      b = (b * (1 - rate)).round
-      compose(a, r, g, b)
-    end
-
-    def with_alpha(color, alpha)
-      (alpha << 24) | (color & 0xffffff)
-    end
-
     def sinoid(x, amplitude = 4)
       x += SINOID_STEP
       x = 0 if x >= FULL_CIRCLE
       [x, amplitude * Math.sin(x)]
-    end
-
-    private
-
-    def components(color)
-      [(color & 0xff000000) >> 24, (color & 0xff0000) >> 16, (color & 0xff00) >> 8, color & 0xff]
-    end
-
-    def compose(a, r, g, b)
-      (a << 24) | (r << 16) | (g << 8) | b
     end
   end
 end
